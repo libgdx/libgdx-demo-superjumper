c284986514b364c7e0da8feb61dc181ea1275abb
clém vicart <clm vicart>
2016-09-08 02:20:16 +0200

    Revert "adding wakelock to keep the screen on"
    This reverts commit a032dcac39101e1ee62d3dabcf7b0b6a5cfd5e78.

--- android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
+++ android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
@@ -11,8 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.hideStatusBar = true;
-		config.useWakelock = true;
 		initialize(new SuperJumper(), config);
 	}
 }
