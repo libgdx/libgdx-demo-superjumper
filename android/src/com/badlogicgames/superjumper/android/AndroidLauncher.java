4665e812cbabbdb89ee0229bf4f0bdffd0450352
clém vicart <clm vicart>
2016-09-08 02:26:40 +0200

    adding wakelock to keep the screen on

--- android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
+++ android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
@@ -11,6 +11,7 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useWakelock = true;
 		initialize(new SuperJumper(), config);
 	}
 }
